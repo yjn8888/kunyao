80bf72072a011255e95ba4630aded1a50e4cfd99
yuanjianing <yuanjianing@hanhua.com>
2018-12-29 12:39:29 +0800

    集成lombok

--- kunyao-rpc/kunyao-rpc-dubbo/kunyao-rpc-dubbo-plugin/src/main/java/com/kunyao/dubbo/filter/trace/ConsumerRpcTraceFilter.java
+++ kunyao-rpc/kunyao-rpc-dubbo/kunyao-rpc-dubbo-plugin/src/main/java/com/kunyao/dubbo/filter/trace/ConsumerRpcTraceFilter.java
@@ -2,8 +2,10 @@ package com.kunyao.dubbo.filter.trace;
 
 import com.kunyao.log.trace.LogConstant;
 import com.kunyao.log.trace.LogTraceSerialContext;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.rpc.*;
 
+@Slf4j
 public class ConsumerRpcTraceFilter implements Filter {
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
@@ -12,10 +14,14 @@ public class ConsumerRpcTraceFilter implements Filter {
     }
 
     private void handleInvokeId(){
-        RpcContext rpcContext = RpcContext.getContext();
-        if(rpcContext!=null){
-            String invokeId = LogTraceSerialContext.handleInvokeId(null);
-            rpcContext.setAttachment(LogConstant.TRACE_ID,invokeId);
+        try {
+            RpcContext rpcContext = RpcContext.getContext();
+            if (rpcContext != null) {
+                String invokeId = LogTraceSerialContext.handleInvokeId(null);
+                rpcContext.setAttachment(LogConstant.TRACE_ID, invokeId);
+            }
+        }catch (Exception e){
+            log.error(e.getMessage(),e);
         }
     }
 }
--- kunyao-rpc/kunyao-rpc-dubbo/kunyao-rpc-dubbo-plugin/src/main/java/com/kunyao/dubbo/filter/trace/ProviderRpcTraceFilter.java
+++ kunyao-rpc/kunyao-rpc-dubbo/kunyao-rpc-dubbo-plugin/src/main/java/com/kunyao/dubbo/filter/trace/ProviderRpcTraceFilter.java
@@ -2,8 +2,10 @@ package com.kunyao.dubbo.filter.trace;
 
 import com.kunyao.log.trace.LogConstant;
 import com.kunyao.log.trace.LogTraceSerialContext;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.rpc.*;
 
+@Slf4j
 public class ProviderRpcTraceFilter implements Filter {
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
@@ -14,10 +16,14 @@ public class ProviderRpcTraceFilter implements Filter {
     }
 
     private void handleInvokeId(){
-        RpcContext rpcContext = RpcContext.getContext();
-        if(rpcContext!=null){
-            String invokeId = rpcContext.getAttachment(LogConstant.TRACE_ID);
-            LogTraceSerialContext.handleInvokeId(invokeId);
+        try {
+            RpcContext rpcContext = RpcContext.getContext();
+            if (rpcContext != null) {
+                String invokeId = rpcContext.getAttachment(LogConstant.TRACE_ID);
+                LogTraceSerialContext.handleInvokeId(invokeId);
+            }
+        }catch (Exception e){
+            log.error(e.getMessage(),e);
         }
     }
 
